bbe2549010250a8e4cac212613ea48d4b378a4cf
Atilla Gündüz <gunduz.atilla@gmail.com>
2013-04-10 14:09:53 +0300

    add sign_in to users_controller

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,12 +49,16 @@ describe "User Pages" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
 
-			describe "after saving the user" do
-				before { click_button submit }
-				let(:user) { User.find_by_email('user@example.com') }
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-			end
+            describe "after saving the user" do
+                before { click_button submit }
+                let(:user) { User.find_by_email('user@example.com') }
+
+                it { should have_selector('title', text: user.name) }
+                it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+#               it { should have_link('Sign out') }
+                it { should have_link('Sign out', href: signout_path) }
+            end
 		end
 	end
 
