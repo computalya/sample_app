2e47bf3d90037bad126ad82deb8ebfbf2b15093c
Atilla Gündüz <gunduz.atilla@gmail.com>
2013-04-18 10:21:03 +0300

    remove a comment

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -56,7 +56,6 @@ describe "User Pages" do
                 it { should have_selector('title', text: user.name) }
                 it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 
-#               it { should have_link('Sign out') }
                 it { should have_link('Sign out', href: signout_path) }
             end
 		end
