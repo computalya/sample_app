6f75bd38479317b308348f3de27b81a4636dcac6
Atilla Gündüz <gunduz.atilla@gmail.com>
2013-04-18 10:23:06 +0300

    add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,20 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
-	
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(params[:user])
+          # Handle a successful update.
+    	else
+          render 'edit'
+        end
+    end
+
 	def new
 	    @user = User.new
   	end
