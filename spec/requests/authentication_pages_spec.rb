2dedc62132424a8098466d5ba46177d3717ff0a2
Atilla Gündüz <gunduz.atilla@gmail.com>
2013-04-18 10:52:45 +0300

    Add Users link

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,6 +36,7 @@ describe "Authentication" do
             it { should have_selector('title', text: user.name) }
             it { should have_link('Profile', href: user_path(user)) }
             it { should have_link('Settings', href: edit_user_path(user)) }
+            it { should have_link('Users',    href: users_path) }
             it { should have_link('Sign out', href: signout_path) }
             it { should_not have_link('Sign in', href: signin_path) }
 
@@ -56,7 +57,7 @@ describe "Authentication" do
                   before { visit users_path }
                   it { should have_selector('title', text: 'Sign in') }
                 end
-                
+
                 describe "visiting the edit page" do
                     before { visit edit_user_path(user) }
 
