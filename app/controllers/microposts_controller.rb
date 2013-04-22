8ef287082e50fb08ce557b39dd6f9a98ed88114c
Atilla Gündüz <gunduz.atilla@gmail.com>
2013-04-22 23:16:30 +0300

    fix errors mit objects

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,8 +5,22 @@ class MicropostsController < ApplicationController
     end
 
     def create
+        @micropost = current_user.microposts.build(params[:micropost])
+        if @micropost.save
+          flash[:success] = "Micropost created!"
+          redirect_to root_path
+            else
+          render 'static_pages/home'
+        end
     end
 
     def destroy
+    end
+
+ private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_path if @micropost.nil?
     end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+   def home
+    if signed_in?
+      @micropost = current_user.microposts.build
+     end
+   end
 
   def help
   end
